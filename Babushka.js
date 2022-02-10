56f0352d824a8cbe9ee1dbec158364e790a38af2
Andreas <andreaslittautimmermann@hotmail.dk>
2022-02-10 22:10:21 +0100

    hej

--- Babushka.js
+++
@@ -1,8 +0,0 @@
-const url = "https://babushka-dd8a.restdb.io/rest/menu";
-
-/* Her skrives nøglen, så vi får adgang til vores data */
-const options = {
-  headers: {
-    "x-apikey": "600ec2fb1346a1524ff12de4",
-  },
-};
